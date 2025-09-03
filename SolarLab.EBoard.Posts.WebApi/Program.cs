96ee2ba63444a01070f90bc54f2097ad85d9ca6c
Ревотюк Даниил <danistollen@yahoo.com>
2025-09-03 22:34:08 +0300

    Basic WebApi and extensions

---
+++ SolarLab.EBoard.Posts.WebApi/Program.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using SolarLab.EBoard.Posts.Application;
+using SolarLab.EBoard.Posts.Infrastructure;
+using SolarLab.EBoard.Posts.WebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+var apiGroup = app.MapGroup("/api");
+app.MapEndpoints(apiGroup);
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MigrateDb();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
