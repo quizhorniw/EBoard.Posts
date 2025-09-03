ef0cd74da3b56f7fed39009f94c4e69ff67a864b
Ревотюк Даниил <danistollen@yahoo.com>
2025-09-03 22:35:01 +0300

    Enable authorization for endpoints

--- SolarLab.EBoard.Posts.Infrastructure/DependencyInjection.cs
+++ SolarLab.EBoard.Posts.Infrastructure/DependencyInjection.cs
@@ -54,6 +54,7 @@ public static class DependencyInjection
                 };
             });
         
+        services.AddAuthorization();
         services.AddHttpContextAccessor();
         services.AddScoped<IUserContext, UserContext>();
 
