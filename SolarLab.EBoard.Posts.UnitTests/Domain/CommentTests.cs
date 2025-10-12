a9010aad03212dd42f311dfbf4006295aa1cb042
Ревотюк Даниил <danistollen@yahoo.com>
2025-10-12 18:23:20 +0300

    Comment entity tests

--- SolarLab.EBoard.Posts.UnitTests/Domain/CommentTests.cs
+++ SolarLab.EBoard.Posts.UnitTests/Domain/CommentTests.cs
@@ -4,8 +4,11 @@ namespace SolarLab.EBoard.Posts.UnitTests.Domain;
 
 public class CommentTests
 {
-    private readonly DateTime _testDateTime = new(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
+    private static readonly Guid TestPostId = Guid.Parse("d2eb359d-9d6c-4f20-b22e-4a9fc91844d5"); 
+    private static readonly Guid TestUserId = Guid.Parse("43bef27b-ca9d-490d-a998-62e7f06f3bf7"); 
+    private const string TestText = "Text";
+    private static readonly DateTime TestDateTime = new(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    
     [Theory]
     [InlineData("d2eb359d-9d6c-4f20-b22e-4a9fc91844d5", "43bef27b-ca9d-490d-a998-62e7f06f3bf7", "Comment 1")]
     [InlineData("8cde7e16-4362-474b-bb65-f7406b2d65b3", "0214c189-43bc-4f06-a5eb-33d7f98799e3", " some-comment-2")]
@@ -17,14 +20,14 @@ public class CommentTests
         var userIdGuid = Guid.Parse(userId);
         
         // Act
-        var result = Comment.Create(postIdGuid, userIdGuid, text, _testDateTime);
+        var result = Comment.Create(postIdGuid, userIdGuid, text, TestDateTime);
         
         // Assert
         Assert.NotEqual(Guid.Empty, result.Id);
         Assert.Equal(postIdGuid, result.PostId);
         Assert.Equal(userIdGuid, result.UserId);
         Assert.Equal(text.Trim(), result.Text);
-        Assert.Equal(_testDateTime, result.CreatedAt);
+        Assert.Equal(TestDateTime, result.CreatedAt);
     }
 
     [Theory]
@@ -36,12 +39,40 @@ public class CommentTests
     public void CreateComment_WithNullOrWhitespaceText_Throws(string text)
     {
         // Arrange
-        var postId = Guid.Parse("8cde7e16-4362-474b-bb65-f7406b2d65b3");
-        var userId = Guid.Parse("43bef27b-ca9d-490d-a998-62e7f06f3bf7");
+        // Act
+        // Assert
+        Assert.Throws<ArgumentException>(() => Comment.Create(TestPostId, TestUserId, text, TestDateTime));
+    }
+
+    [Theory]
+    [InlineData("title-1")]
+    [InlineData("Another - title - 2")]
+    [InlineData("  TiTlE 3 ")]
+    public void ChangeCommentText_WithValidNewText_ChangesCommentText(string newText)
+    {
+        // Arrange
+        var comment = Comment.Create(TestPostId, TestUserId, TestText, TestDateTime);
+        
+        // Act
+        comment.ChangeText(newText);
+        
+        // Assert
+        Assert.Equal(newText.Trim(), comment.Text);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\n")]
+    [InlineData(" \n\r  \t")]
+    public void ChangeCommentText_WithNullOrWhitespaceNewText_Throws(string newText)
+    {
+        // Arrange
+        var comment = Comment.Create(TestPostId, TestUserId, TestText, TestDateTime);
         
         // Act
         // Assert
-        Assert.Throws<ArgumentException>(() => Comment.Create(postId, userId, text, _testDateTime));
+        Assert.Throws<ArgumentException>(() => comment.ChangeText(newText));
     }
-    
 }
